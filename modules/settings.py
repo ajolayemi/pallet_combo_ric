be8958735b044d599e9d2589bb47066b98bae02f
jolayemi_local <ajolayemi82@gmail.com>
2021-09-08 15:22:00 +0200

    Defined various constants that stores information related to the database that will be used in this project.

--- modules/settings.py
+++ modules/settings.py
@@ -8,5 +8,14 @@ INFORMATION_JSON = '../app_info_json.json'
 TO_DO_COMBO_ITEMS = ['Sì', 'No']
 GOOGLE_SHEET_WB_NAME = 'Feed Algoritmo per PED'
 
+# Database constants
+DATABASE_NAME = 'info_pedane.sqlite'
+PALLET_INFO_TABLE = 'Pallets'
+CLIENT_INFO_TABLE = 'Clients'
+CONNECTION_NAME = f'{helper_functions.get_user_name()}_Writer'
+DATABASE_DRIVER = 'QSQLITE'
+READER_CONNECTION_NAME = f'{helper_functions.get_user_name()}_Reader'
+
+
 if __name__ == '__main__':
     pass
