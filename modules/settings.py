99a7d1973ffd9cafe938ac2673c63395d0e996f7
jolayemi_local <ajolayemi82@gmail.com>
2021-09-08 12:44:33 +0200

    Defined TO_DO_COMBO_ITEMS and GOOGLE_SHEET_WB_NAME

--- modules/settings.py
+++ modules/settings.py
@@ -4,6 +4,8 @@ functioning of this algorithm. """
 from helper_modules import helper_functions
 
 INFORMATION_JSON = '../app_info_json.json'
+TO_DO_COMBO_ITEMS = ['Sì', 'No']
+GOOGLE_SHEET_WB_NAME = 'Feed Algoritmo per PED'
 
 if __name__ == '__main__':
     pass
