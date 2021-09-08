1f5945f6661d41bd723458b97cbe57961079a256
jolayemi_local <ajolayemi82@gmail.com>
2021-09-08 12:03:12 +0200

    Imported settings module and helper_functions from helper_modules and defined APP_INFO_JSON_CONTENTS variable

--- modules/main_window.py
+++ modules/main_window.py
@@ -7,9 +7,19 @@ from PyQt5.QtWidgets import (QApplication, QLabel,
 
 from PyQt5.QtGui import QFont
 
-
+# Self defined modules
+from helper_modules import helper_functions
+import settings
 MSG_FONT = QFont('Italics', 13)
+
+APP_INFO_JSON_CONTENTS = helper_functions.json_file_loader(
+    file_name=settings.INFORMATION_JSON
+)
 
 
 class MainPage(QMainWindow):
     """" User GUI. """
+
+
+if __name__ == '__main__':
+    pass
