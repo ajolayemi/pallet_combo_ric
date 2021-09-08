f74367c4d07181e455028689bdd6c2211baebc1c
jolayemi_local <ajolayemi82@gmail.com>
2021-09-08 15:29:38 +0200

    Added shebang line Added script description Imported the needed modules

---
+++ modules/db_communicator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+""" Communicates with the database that stores some necessary
+information needed in this project. """
+
+
+from PyQt5.QtSql import QSqlQuery, QSqlDatabase
+
+# self defined modules
+import settings
+from helper_modules import helper_functions
+
