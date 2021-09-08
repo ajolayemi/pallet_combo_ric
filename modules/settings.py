3bdba34f5f95fa65a163ac71990acb286658a26d
jolayemi_local <ajolayemi82@gmail.com>
2021-09-08 12:01:08 +0200

    Defined INFORMATION_JSON constant

---
+++ modules/settings.py
@@ -0,0 +1,9 @@
+""" Contains some information necessary for the correct
+functioning of this algorithm. """
+
+from helper_modules import helper_functions
+
+INFORMATION_JSON = '../app_info_json.json'
+
+if __name__ == '__main__':
+    pass
