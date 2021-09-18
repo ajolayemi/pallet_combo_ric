e39e79f7436679d3d6f2613d5396d480c7a1e123
jolayemi_local <ajolayemi82@gmail.com>
2021-09-18 14:40:57 +0200

    final touch

--- modules/api_communicator.py
+++ modules/api_communicator.py
@@ -477,7 +477,4 @@ class PedApi(QObject):
 
 
 if __name__ == '__main__':
-    order_link = \
-        'https://docs.google.com/spreadsheets/d/1umjpTeSty4h6IGnaexrlNyV9b0vPWmif551E7P4hoMI/edit#gid=2110154666'
-    test = PedApi(order_spreadsheet=order_link, overwrite_data=True, for_pallets=True)
-    print(test.construct_pallets())
+    pass
